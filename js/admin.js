a1a6c50b2d57de5a27d51424f4b3f4da771de141
Mohamed Yassin <mohamed.aladin.yassin@gmail.com>
2017-11-21 16:27:12 -0500

    Hide cart after hiding

--- js/admin.js
+++ js/admin.js
@@ -1078,6 +1078,7 @@ function archiveCart(cartNumber) {
     oldRef.once('value').then(function (snapshot) {
         archivedCartsRef.set(snapshot.val()).then(function () {
             oldRef.remove();
+            $('#cartViewModal').modal('hide');
         })
     });
 }
